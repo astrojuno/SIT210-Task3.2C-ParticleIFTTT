1b72655ecd7157767cc9cb337d9f4306a46c6cce
astrojuno <39887497+astrojuno@users.noreply.github.com>
2021-04-02 15:54:03 +1100

    Update 32clightsensor.ino

--- 32clightsensor.ino
+++ 32clightsensor.ino
@@ -6,11 +6,13 @@ BH1750Lib lightSensor;
 
 // the value that we will consider full sunlight
 int sunlight = 5000;
+int averageSize = 3;
 int lastThreeValues[3];
 int nextIndex = 0;
 bool wasDark = true;
 // IFTTT event name
-String event = "sunlight";
+String sunlightEvent = "sunlight";
+String darknessEvent = "darkness";
 // IFTTT recipe data
 String eventDataSunlight = "light";
 String eventDataDarkness = "dark";
@@ -32,6 +34,7 @@ void setup(){
 void loop() {
   // take the reading
   int lux = lightSensor.lightLevel();
+  
   // print to serial so you can keep track
   Serial.print("Light: ");
   Serial.print(lux);
@@ -45,19 +48,36 @@ void loop() {
   // we're going to average the last three values to combat
   // against a flash of light
   lastThreeValues[nextIndex] = lux;
+  int luxAverage = 0;
+  for (int i = 0; i < averageSize; i++) {
+    luxAverage += lastThreeValues[i];
+  }
+  luxAverage = luxAverage / averageSize;
+  
+  Serial.println("i = " + String(nextIndex));
+  Serial.println("average = " + String(luxAverage));
+  Serial.print("lux values: ");
+  for (int i = 0; i < averageSize; i++) {
+      Serial.print(String(lastThreeValues[i]) + "; ");
+  }
+  Serial.println();
+  
+  
+  if (isSunlight(luxAverage) && wasDark) {
+      wasDark = false;
+      // send notification that sunlight is on
+      Particle.publish(sunlightEvent, eventDataSunlight, 60, PRIVATE);
+  } else if (!isSunlight(lux) && !wasDark) {
+      wasDark = true;
+      //send notification that sunlight is of
+      //Particle.publish(event, eventDataDarkness, 60, PRIVATE);
+      Particle.publish(darknessEvent, eventDataDarkness, 60, PRIVATE);
+  }
+  
   nextIndex++;
   
   if (nextIndex > 2) {
       nextIndex = 0;
-      if (isSunlight(lux) && wasDark) {
-          wasDark = false;
-          // send notification that sunlight is on
-          Particle.publish(event, eventDataSunlight);
-      } else if (!isSunlight(lux) && !wasDark) {
-          wasDark = true;
-          // send notification that sunilght is off
-          Particle.publish(event, eventDataDarkness);
-      }
   }
   
   // Take a reading every specified time
